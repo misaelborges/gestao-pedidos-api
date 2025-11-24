2213ba8cdb6a213fe137f7fd1cef6ab6592049d1
Misael Borges <mizaelborges2011@gmail.com>
2025-11-24 14:51:41 -0300

    first commit

---
+++ src/main/java/com/datum/gestao/pedidos/GestaoPedidosApiApplication.java
@@ -0,0 +1,13 @@
+package com.datum.gestao.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPedidosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPedidosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datum/gestao/pedidos/GestaoPedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datum.gestao.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
