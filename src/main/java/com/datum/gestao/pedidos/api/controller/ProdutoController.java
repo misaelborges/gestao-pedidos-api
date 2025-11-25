fb294e3a9372d554170199ae0944f27961dbdbfa
Misael Borges <mizaelborges2011@gmail.com>
2025-11-25 14:49:11 -0300

    feat: cria ProdutoController com injeção de dependência de ProdutoService

---
+++ src/main/java/com/datum/gestao/pedidos/api/controller/ProdutoController.java
@@ -0,0 +1,16 @@
+package com.datum.gestao.pedidos.api.controller;
+
+import com.datum.gestao.pedidos.domain.service.ProdutoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+}
