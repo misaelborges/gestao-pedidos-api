d00bbc44cc18b4ec06c8cb599f83e107b0d685df
Misael Borges <mizaelborges2011@gmail.com>
2025-11-25 14:33:42 -0300

    feat: adiciona ProdutoRepository

---
+++ src/main/java/com/datum/gestao/pedidos/domain/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.datum.gestao.pedidos.domain.repository;
+
+import com.datum.gestao.pedidos.domain.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
