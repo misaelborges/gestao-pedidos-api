cedfafbf60bd42b449f602c46f20768840390150
Misael Borges <mizaelborges2011@gmail.com>
2025-11-25 14:49:27 -0300

    feat: cria PedidoController com injeção de dependência de PedidoService

---
+++ src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
@@ -0,0 +1,16 @@
+package com.datum.gestao.pedidos.api.controller;
+
+import com.datum.gestao.pedidos.domain.service.PedidoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pedido")
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+}
