0888929b72efa7fab077cbe88e130b8307c34238
Misael Borges <mizaelborges2011@gmail.com>
2025-11-25 14:48:56 -0300

    feat: cria ClienteController com injeção de dependência de ClienteService

---
+++ src/main/java/com/datum/gestao/pedidos/api/controller/ClienteController.java
@@ -0,0 +1,16 @@
+package com.datum.gestao.pedidos.api.controller;
+
+import com.datum.gestao.pedidos.domain.service.ClienteService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+}
