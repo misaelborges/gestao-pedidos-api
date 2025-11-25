3a88c12c07ffbdc3ff497036bca72aa578ebd6d6
Misael Borges <mizaelborges2011@gmail.com>
2025-11-25 14:50:52 -0300

    refactor: muda a rota de /pedido para /pedidos para ficar padrão com os outros controllers

--- src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
+++ src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pedido")
+@RequestMapping("/pedidos")
 public class PedidoController {
 
     private final PedidoService pedidoService;
