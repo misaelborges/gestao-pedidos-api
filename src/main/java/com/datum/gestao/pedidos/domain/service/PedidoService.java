98e953eae8f663b4f0431d161e0686601e15601b
Misael Borges <mizaelborges2011@gmail.com>
2025-11-25 14:45:14 -0300

    feat: cria PedidoService com injeção de dependência de PedidoRepository

---
+++ src/main/java/com/datum/gestao/pedidos/domain/service/PedidoService.java
@@ -0,0 +1,14 @@
+package com.datum.gestao.pedidos.domain.service;
+
+import com.datum.gestao.pedidos.domain.repository.PedidoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    private final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+}
