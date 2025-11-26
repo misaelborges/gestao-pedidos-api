f0df639b1eb5e99b86da966f6691c23f8b0f0b45
Misael Borges <mizaelborges2011@gmail.com>
2025-11-26 15:41:47 -0300

    feat: adiciona controller para buscar cliente por id

--- src/main/java/com/datum/gestao/pedidos/api/controller/ClienteController.java
+++ src/main/java/com/datum/gestao/pedidos/api/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.datum.gestao.pedidos.api.controller;
 
+import com.datum.gestao.pedidos.api.dto.cliente.ClienteResponseDTO;
 import com.datum.gestao.pedidos.api.dto.cliente.ClienteResumoResponseDTO;
 import com.datum.gestao.pedidos.domain.service.ClienteService;
 import org.springframework.data.domain.Page;
@@ -7,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,11 @@ public class ClienteController {
     public ResponseEntity<Page<ClienteResumoResponseDTO>> listarClientes(Pageable pageable) {
         Page<ClienteResumoResponseDTO> clientePage = clienteService.listarClientes(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(clientePage);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteResponseDTO> buscarClientePorId(@PathVariable Long id) {
+        ClienteResponseDTO clienteResponseDTO = clienteService.buscarClientePorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(clienteResponseDTO);
     }
 }
