7e592e33d8f93d3a42e71f48a1bb84e02d558b5b
Misael Borges <mizaelborges2011@gmail.com>
2025-11-26 15:38:52 -0300

    feat: adiciona exceção base de negócio

---
+++ src/main/java/com/datum/gestao/pedidos/domain/exception/NegocioException.java
@@ -0,0 +1,16 @@
+package com.datum.gestao.pedidos.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class NegocioException extends RuntimeException{
+
+    public NegocioException(String message) {
+        super(message);
+    }
+
+    public NegocioException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
