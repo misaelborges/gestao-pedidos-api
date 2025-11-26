36c7b4f1ffaaa013628a7ca1bb49097832be6367
Misael Borges <mizaelborges2011@gmail.com>
2025-11-26 16:40:05 -0300

    feat: adiciona metodo para atualizar um cliente existente

--- src/main/java/com/datum/gestao/pedidos/domain/service/ClienteService.java
+++ src/main/java/com/datum/gestao/pedidos/domain/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.datum.gestao.pedidos.domain.service;
 
+import com.datum.gestao.pedidos.api.dto.cliente.ClienteAtualizaRequestDTO;
 import com.datum.gestao.pedidos.api.dto.cliente.ClienteRequestDTO;
 import com.datum.gestao.pedidos.api.dto.cliente.ClienteResponseDTO;
 import com.datum.gestao.pedidos.api.dto.cliente.ClienteResumoResponseDTO;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -43,7 +45,26 @@ public class ClienteService {
         return clienteMapper.toClienteResponseDTO(clienteSalvo);
     }
 
-    private Cliente buscadorClinte(Long id) {
+    @Transactional
+    public ClienteResponseDTO atualizarClientePorId(Long id, ClienteAtualizaRequestDTO clienteAtualizaRequestDTO) {
+        Cliente clienteBanco = buscarCliente(id);
+
+        atualizarCliente(clienteAtualizaRequestDTO, clienteBanco);
+
+        return clienteMapper.toClienteResponseDTO(clienteBanco);
+    }
+
+    private void atualizarCliente(ClienteAtualizaRequestDTO clienteAtualizaRequestDTO, Cliente clienteBanco) {
+        clienteBanco.setNome(clienteAtualizaRequestDTO.nome());
+        clienteBanco.setTelefone(clienteAtualizaRequestDTO.telefone());
+        clienteBanco.setLogradouro(clienteAtualizaRequestDTO.logradouro());
+        clienteBanco.setNumero(clienteAtualizaRequestDTO.numero());
+        clienteBanco.setComplemento(clienteAtualizaRequestDTO.complemento());
+        clienteBanco.setBairro(clienteAtualizaRequestDTO.bairro());
+        clienteBanco.setCidade(clienteAtualizaRequestDTO.cidade());
+        clienteBanco.setEstado(clienteAtualizaRequestDTO.estado());
+    }
+
     private Cliente buscarCliente(Long id) {
         return clienteRepository.findById(id).orElseThrow(() -> new ClienteNaoEncontradoException(id));
     }
