4884ee10882ae9e2fc69342e382872e5ea1ba29b
Misael Borges <mizaelborges2011@gmail.com>
2025-11-26 15:55:01 -0300

    feat: adiciona controller para salvar um cliente

--- src/main/java/com/datum/gestao/pedidos/api/controller/ClienteController.java
+++ src/main/java/com/datum/gestao/pedidos/api/controller/ClienteController.java
@@ -1,16 +1,15 @@
 package com.datum.gestao.pedidos.api.controller;
 
+import com.datum.gestao.pedidos.api.dto.cliente.ClienteRequestDTO;
 import com.datum.gestao.pedidos.api.dto.cliente.ClienteResponseDTO;
 import com.datum.gestao.pedidos.api.dto.cliente.ClienteResumoResponseDTO;
 import com.datum.gestao.pedidos.domain.service.ClienteService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
@@ -32,5 +31,11 @@ public class ClienteController {
     public ResponseEntity<ClienteResponseDTO> buscarClientePorId(@PathVariable Long id) {
         ClienteResponseDTO clienteResponseDTO = clienteService.buscarClientePorId(id);
         return ResponseEntity.status(HttpStatus.OK).body(clienteResponseDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteResponseDTO> salvarCliente(@RequestBody @Valid ClienteRequestDTO clienteRequestDTO) {
+        ClienteResponseDTO clienteResponseDTO = clienteService.salvarCliente(clienteRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteResponseDTO);
     }
 }
