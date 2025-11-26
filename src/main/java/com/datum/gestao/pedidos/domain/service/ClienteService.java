11b8c0742a1647826ad94f1564ac6781818d7c9c
Misael Borges <mizaelborges2011@gmail.com>
2025-11-26 15:21:33 -0300

    feat: adiciona metodo para listar todos os clientes

--- src/main/java/com/datum/gestao/pedidos/domain/service/ClienteService.java
+++ src/main/java/com/datum/gestao/pedidos/domain/service/ClienteService.java
@@ -1,14 +1,30 @@
 package com.datum.gestao.pedidos.domain.service;
 
+import com.datum.gestao.pedidos.api.dto.cliente.ClienteResumoResponseDTO;
+import com.datum.gestao.pedidos.core.mapper.ClienteMapper;
+import com.datum.gestao.pedidos.domain.model.Cliente;
 import com.datum.gestao.pedidos.domain.repository.ClienteRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
 
     private final ClienteRepository clienteRepository;
+    private final ClienteMapper clienteMapper;
 
-    public ClienteService(ClienteRepository clienteRepository) {
+    public ClienteService(ClienteRepository clienteRepository, ClienteMapper clienteMapper) {
         this.clienteRepository = clienteRepository;
+        this.clienteMapper = clienteMapper;
+    }
+
+    public Page<ClienteResumoResponseDTO> listarClientes(Pageable pageable) {
+        Page<Cliente> clientes = clienteRepository.findAll(pageable);
+        List<ClienteResumoResponseDTO> dtos = clienteMapper.toClienteResumoDTO(clientes.getContent());
+        return new PageImpl<>(dtos, pageable, clientes.getTotalElements());
     }
 }
