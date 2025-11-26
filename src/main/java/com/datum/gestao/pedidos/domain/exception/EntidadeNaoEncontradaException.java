25c8690742c4875299cb8cc8b7306aa8fb4bea2e
Misael Borges <mizaelborges2011@gmail.com>
2025-11-26 15:39:28 -0300

    feat: adiciona exceção genéria de entidade não encontrada

---
+++ src/main/java/com/datum/gestao/pedidos/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,13 @@
+package com.datum.gestao.pedidos.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class EntidadeNaoEncontradaException extends NegocioException {
+
+    public EntidadeNaoEncontradaException(String mensagem) {
+        super(mensagem);
+    }
+
+}
