9383efb22c06ab6c4835a56de6b4db76b2259222
Misael Borges <mizaelborges2011@gmail.com>
2025-11-26 15:21:45 -0300

    feat: adiciona controller para listar todos os clientes

--- src/main/java/com/datum/gestao/pedidos/api/controller/ClienteController.java
+++ src/main/java/com/datum/gestao/pedidos/api/controller/ClienteController.java
@@ -1,6 +1,12 @@
 package com.datum.gestao.pedidos.api.controller;
 
+import com.datum.gestao.pedidos.api.dto.cliente.ClienteResumoResponseDTO;
 import com.datum.gestao.pedidos.domain.service.ClienteService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,11 @@ public class ClienteController {
 
     public ClienteController(ClienteService clienteService) {
         this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClienteResumoResponseDTO>> listarClientes(Pageable pageable) {
+        Page<ClienteResumoResponseDTO> clientePage = clienteService.listarClientes(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(clientePage);
     }
 }
