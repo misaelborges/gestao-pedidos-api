654faef1f890881369c8581bffdf1bb43129584c
Misael Borges <mizaelborges2011@gmail.com>
2025-11-27 15:09:12 -0300

    refector: adicona validação para o ProdutoAtualizaRequestDTO

--- src/main/java/com/datum/gestao/pedidos/api/dto/produto/ProdutoAtualizaRequestDTO.java
+++ src/main/java/com/datum/gestao/pedidos/api/dto/produto/ProdutoAtualizaRequestDTO.java
@@ -1,11 +1,26 @@
 package com.datum.gestao.pedidos.api.dto.produto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.math.BigDecimal;
 
 public record ProdutoAtualizaRequestDTO(
+
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
+
         String descricao,
+
+        @NotNull(message = "Preço é obrigatório")
+        @Positive(message = "Preço deve ser maior que zero")
         BigDecimal preco,
+
+        @NotNull(message = "Estoque é obrigatório")
+        @PositiveOrZero(message = "Estoque não pode ser negativo")
         Integer estoqueDisponivel,
+
         String categoria
 ) {}
