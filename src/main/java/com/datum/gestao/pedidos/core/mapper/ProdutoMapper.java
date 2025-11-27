59cc930aedc45c80f24a2a35c084978c32f884f4
Misael Borges <mizaelborges2011@gmail.com>
2025-11-27 14:50:36 -0300

    feat: cria ProdutoMapper

---
+++ src/main/java/com/datum/gestao/pedidos/core/mapper/ProdutoMapper.java
@@ -0,0 +1,17 @@
+package com.datum.gestao.pedidos.core.mapper;
+
+import com.datum.gestao.pedidos.api.dto.produto.ProdutoRequestDTO;
+import com.datum.gestao.pedidos.api.dto.produto.ProdutoResponseDTO;
+import com.datum.gestao.pedidos.api.dto.produto.ProdutoResumoResponseDTO;
+import com.datum.gestao.pedidos.domain.model.Produto;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProdutoMapper {
+
+    Produto toProduto(ProdutoRequestDTO produtoRequest);
+    List<ProdutoResumoResponseDTO> toProdutoResumoDTO(List<Produto> produtos);
+    ProdutoResponseDTO toProdutoResponseDTO(Produto produto);
+}
