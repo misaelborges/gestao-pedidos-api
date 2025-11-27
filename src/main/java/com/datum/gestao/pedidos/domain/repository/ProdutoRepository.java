b509fd33e9fce41f4234b701b09c6b4c8d19cc27
Misael Borges <mizaelborges2011@gmail.com>
2025-11-27 17:20:19 -0300

    refector: remove os antigos metodos que não estava mais sendo usados

--- src/main/java/com/datum/gestao/pedidos/domain/repository/ProdutoRepository.java
+++ src/main/java/com/datum/gestao/pedidos/domain/repository/ProdutoRepository.java
@@ -1,13 +1,11 @@
 package com.datum.gestao.pedidos.domain.repository;
 
 import com.datum.gestao.pedidos.domain.model.Produto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long>, JpaSpecificationExecutor<Produto> {
 
-    Page<Produto> findAllProdutoByAtivoTrue(Pageable pageable);
 }
