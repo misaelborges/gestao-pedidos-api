fdce1b09c43ee831daa475a0d3e183580f4b166d
Misael Borges <mizaelborges2011@gmail.com>
2025-11-27 14:56:49 -0300

    feat: cria ProdutoAssembler

---
+++ src/main/java/com/datum/gestao/pedidos/api/assembler/ProdutoAssembler.java
@@ -0,0 +1,47 @@
+package com.datum.gestao.pedidos.api.assembler;
+
+import com.datum.gestao.pedidos.api.controller.ProdutoController;
+import com.datum.gestao.pedidos.api.dto.produto.ProdutoResponseDTO;
+import com.datum.gestao.pedidos.api.dto.produto.ProdutoResumoResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class ProdutoAssembler {
+
+    public EntityModel<ProdutoResponseDTO> toProtudoResponseDTO(ProdutoResponseDTO produtoResponseDTO) {
+        return EntityModel.of(produtoResponseDTO)
+                .add(linkTo(methodOn(ProdutoController.class)
+                        .buscarProdutoPorId(produtoResponseDTO.id())).withSelfRel());
+    }
+
+    public PagedModel<EntityModel<ProdutoResumoResponseDTO>> toProdutoResumoDTO(Page<ProdutoResumoResponseDTO> dtoList) {
+        List<EntityModel<ProdutoResumoResponseDTO>> produtosComLink = dtoList.stream()
+                .map(dto ->
+                        EntityModel.of(dto).add(linkTo(methodOn(ProdutoController.class)
+                                .buscarProdutoPorId(dto.id())).withSelfRel()))
+                .toList();
+
+        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(
+                dtoList.getSize(),
+                dtoList.getNumber(),
+                dtoList.getTotalElements(),
+                dtoList.getTotalPages()
+        );
+
+        PagedModel<EntityModel<ProdutoResumoResponseDTO>> pagedModel =
+                PagedModel.of(produtosComLink, pageMetadata);
+
+        pagedModel.add(linkTo(methodOn(ProdutoController.class)
+                .listarProdutos(dtoList.getPageable())).withSelfRel());
+
+        return pagedModel;
+    }
+}
