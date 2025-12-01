87dee4553fd911c96c1f953fa064b863efac1dbc
Misael Borges <mizaelborges2011@gmail.com>
2025-12-01 13:41:50 -0300

    refector: cria contrutor, get, set, equals and hashcode, e um metodo para calcularSubtotal

--- src/main/java/com/datum/gestao/pedidos/domain/model/ItemPedido.java
+++ src/main/java/com/datum/gestao/pedidos/domain/model/ItemPedido.java
@@ -3,6 +3,7 @@ package com.datum.gestao.pedidos.domain.model;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 public class ItemPedido {
@@ -27,4 +28,80 @@ public class ItemPedido {
 
     @Column(nullable = false)
     private BigDecimal subtotal;
+
+    public ItemPedido() {
+    }
+
+    public ItemPedido(BigDecimal subtotal, BigDecimal precoUnitario, Integer quantidade, Produto produto, Pedido pedido, Long id) {
+        this.subtotal = subtotal;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+        this.produto = produto;
+        this.pedido = pedido;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal calcularSubTotal(int quandide, BigDecimal precoUnitario) {
+        return precoUnitario.multiply(BigDecimal.valueOf(quandide));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemPedido that = (ItemPedido) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
