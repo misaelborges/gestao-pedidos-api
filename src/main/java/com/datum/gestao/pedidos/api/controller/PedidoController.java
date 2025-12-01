4670034c7c5d63cc5c9850e856d6a44fe6acd501
Misael Borges <mizaelborges2011@gmail.com>
2025-12-01 13:43:25 -0300

    feat: cria controller para criarPedido

--- src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
+++ src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
@@ -1,8 +1,12 @@
 package com.datum.gestao.pedidos.api.controller;
 
+import com.datum.gestao.pedidos.api.dto.pedido.PedidoRequestDTO;
+import com.datum.gestao.pedidos.api.dto.pedido.PedidoResponseDTO;
 import com.datum.gestao.pedidos.domain.service.PedidoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -12,5 +16,16 @@ public class PedidoController {
 
     public PedidoController(PedidoService pedidoService) {
         this.pedidoService = pedidoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PedidoResponseDTO> criarPedido(@RequestBody @Valid PedidoRequestDTO pedidoRequestDTO) {
+        PedidoResponseDTO pedidoResponseDTO = pedidoService.criarPedido(pedidoRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoResponseDTO);
+    }
+
+    @GetMapping("/teste")
+    public String teste() {
+        return "ok";
     }
 }
