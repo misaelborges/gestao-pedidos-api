fd4409355aee3561ca8ef5763e1f2131222aeb6a
Misael Borges <mizaelborges2011@gmail.com>
2025-12-02 14:52:16 -0300

    feat: cria os filtros para usar na listagem de pedidos

--- src/main/java/com/datum/gestao/pedidos/domain/specification/PedidoEspecification.java
+++ src/main/java/com/datum/gestao/pedidos/domain/specification/PedidoEspecification.java
@@ -1,7 +1,10 @@
 package com.datum.gestao.pedidos.domain.specification;
 
 import com.datum.gestao.pedidos.domain.model.Pedido;
+import com.datum.gestao.pedidos.domain.model.StatusPedido;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDateTime;
 
 public class PedidoEspecification {
 
@@ -19,7 +22,39 @@ public class PedidoEspecification {
                         );
     }
 
-    public static Specification<Pedido> ativoTrue() {
-        return (root, query, cb) -> cb.isTrue(root.get("ativo"));
+    public static Specification<Pedido> nomeContem(String nome) {
+        return (root, query, cb) ->
+                nome == null ? null :
+                        cb.like(
+                                cb.lower(root.get("cliente").get("nome")),
+                                "%" + nome.toLowerCase() + "%"
+                        );
+    }
+
+
+    public static Specification<Pedido> comStatus(String status) {
+        return (root, query, cb) ->
+                status == null ? null :
+                        cb.equal(
+                                root.get("status"),
+                                StatusPedido.valueOf(status)
+                        );
+    }
+
+
+    public static Specification<Pedido> comCliente(String cliente) {
+        return (root, query, cb) ->
+                cliente == null ? null :
+                        cb.like(
+                                cb.lower(root.get("cliente").get("nome")),
+                                "%" + cliente.toLowerCase() + "%"
+                        );
+    }
+
+
+    public static Specification<Pedido> comData(LocalDateTime data) {
+        return (root, query, cb) ->
+                data == null ? null :
+                        cb.equal(root.get("data"), data);
     }
 }
