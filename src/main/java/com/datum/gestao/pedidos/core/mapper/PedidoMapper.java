d9cddeed6a0ba192dfa4b4d5637c17677c118a19
Misael Borges <mizaelborges2011@gmail.com>
2025-12-02 14:52:57 -0300

    feat: cria metodo para converter List<Pedido> para List<PedidoResumoResponseDTO>

--- src/main/java/com/datum/gestao/pedidos/core/mapper/PedidoMapper.java
+++ src/main/java/com/datum/gestao/pedidos/core/mapper/PedidoMapper.java
@@ -2,9 +2,13 @@ package com.datum.gestao.pedidos.core.mapper;
 
 import com.datum.gestao.pedidos.api.dto.pedido.PedidoRequestDTO;
 import com.datum.gestao.pedidos.api.dto.pedido.PedidoResponseDTO;
+import com.datum.gestao.pedidos.api.dto.pedido.PedidoResumoResponseDTO;
+import com.datum.gestao.pedidos.domain.model.Cliente;
 import com.datum.gestao.pedidos.domain.model.Pedido;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PedidoMapper {
@@ -13,4 +17,10 @@ public interface PedidoMapper {
 
     @Mapping(source = "cliente.nome", target = "cliente")
     PedidoResponseDTO toPedidoResponseDTO(Pedido pedido);
+
+    List<PedidoResumoResponseDTO> toPedidoResumoDTO(List<Pedido> pedidos);
+
+    default String map(Cliente cliente) {
+        return cliente != null ? cliente.getNome() : null;
+    }
 }
