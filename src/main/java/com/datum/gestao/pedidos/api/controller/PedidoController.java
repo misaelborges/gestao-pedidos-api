c5001b65c0dbb1008dee2e5a502562bf8f091b12
Misael Borges <mizaelborges2011@gmail.com>
2025-12-02 14:53:56 -0300

    feat: cria controller para buscarComFiltros

--- src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
+++ src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
@@ -1,12 +1,18 @@
 package com.datum.gestao.pedidos.api.controller;
 
+import com.datum.gestao.pedidos.api.dto.filtro.PedidoFiltro;
 import com.datum.gestao.pedidos.api.dto.pedido.PedidoRequestDTO;
 import com.datum.gestao.pedidos.api.dto.pedido.PedidoResponseDTO;
+import com.datum.gestao.pedidos.api.dto.pedido.PedidoResumoResponseDTO;
 import com.datum.gestao.pedidos.domain.service.PedidoService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -35,5 +41,17 @@ public class PedidoController {
     public ResponseEntity<PedidoResponseDTO> buscarPedidoId(@PathVariable Long id) {
         PedidoResponseDTO pedidoResponseDTO = pedidoService.buscarPedidoId(id);
         return ResponseEntity.status(HttpStatus.OK).body(pedidoResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PedidoResumoResponseDTO>> buscarComFiltros(
+            @RequestParam(required = false) String nome, @RequestParam(required = false) String status,
+            @RequestParam(required = false) String cliente, @RequestParam(required = false) LocalDateTime data,
+            Pageable pageable) {
+
+        PedidoFiltro filtro = new PedidoFiltro(nome, status, cliente, data);
+        Page<PedidoResumoResponseDTO> pedidoResumoResponseDTOS = pedidoService.buscarComFiltros(filtro, pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(pedidoResumoResponseDTOS);
     }
 }
