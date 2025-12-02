45a3503dda299792f848f346034167326a0c3ab6
Misael Borges <mizaelborges2011@gmail.com>
2025-12-02 15:39:20 -0300

    refector: retira o nome do filtro

--- src/main/java/com/datum/gestao/pedidos/api/dto/filtro/PedidoFiltro.java
+++ src/main/java/com/datum/gestao/pedidos/api/dto/filtro/PedidoFiltro.java
@@ -3,7 +3,6 @@ package com.datum.gestao.pedidos.api.dto.filtro;
 import java.time.LocalDateTime;
 
 public record PedidoFiltro(
-        String nome,
         String status,
         String cliente,
         LocalDateTime data
