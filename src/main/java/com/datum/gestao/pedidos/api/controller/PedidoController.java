665d3d0530970d8bbe6dc6adb64bef8df4187925
Misael Borges <mizaelborges2011@gmail.com>
2025-12-02 15:45:27 -0300

    refector: adiciona paginação nas rotas

--- src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
+++ src/main/java/com/datum/gestao/pedidos/api/controller/PedidoController.java
@@ -1,5 +1,6 @@
 package com.datum.gestao.pedidos.api.controller;
 
+import com.datum.gestao.pedidos.api.assembler.PedidoAssembler;
 import com.datum.gestao.pedidos.api.dto.filtro.PedidoFiltro;
 import com.datum.gestao.pedidos.api.dto.pedido.PedidoRequestDTO;
 import com.datum.gestao.pedidos.api.dto.pedido.PedidoResponseDTO;
@@ -8,6 +9,8 @@ import com.datum.gestao.pedidos.domain.service.PedidoService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,39 +22,44 @@ import java.time.LocalDateTime;
 public class PedidoController {
 
     private final PedidoService pedidoService;
+    private final PedidoAssembler pedidoAssembler;
 
-    public PedidoController(PedidoService pedidoService) {
+    public PedidoController(PedidoService pedidoService, PedidoAssembler pedidoAssembler) {
         this.pedidoService = pedidoService;
+        this.pedidoAssembler = pedidoAssembler;
     }
 
     @PostMapping
-    public ResponseEntity<PedidoResponseDTO> criarPedido(@RequestBody @Valid PedidoRequestDTO pedidoRequestDTO) {
+    public ResponseEntity<EntityModel<PedidoResponseDTO>> criarPedido(@RequestBody @Valid PedidoRequestDTO pedidoRequestDTO) {
         PedidoResponseDTO pedidoResponseDTO = pedidoService.criarPedido(pedidoRequestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoResponseDTO);
+        EntityModel<PedidoResponseDTO> pedidoResponseDTO1 = pedidoAssembler.toPedidoResponseDTO(pedidoResponseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoResponseDTO1);
     }
 
     @GetMapping("/buscar")
-    public ResponseEntity<PedidoResponseDTO> buscarPedido(@RequestParam(required = false) Long pedidoId,
+    public ResponseEntity<EntityModel<PedidoResponseDTO>> buscarPedido(@RequestParam(required = false) Long pedidoId,
                                                           @RequestParam(required = false) String numeroPedido) {
         PedidoResponseDTO pedidoResponseDTO = pedidoService.buscarPedido(pedidoId, numeroPedido);
-        return ResponseEntity.status(HttpStatus.OK).body(pedidoResponseDTO);
+        EntityModel<PedidoResponseDTO> pageResponseDTO = pedidoAssembler.toPedidoResponseDTO(pedidoResponseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pageResponseDTO);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PedidoResponseDTO> buscarPedidoId(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<PedidoResponseDTO>> buscarPedidoId(@PathVariable Long id) {
         PedidoResponseDTO pedidoResponseDTO = pedidoService.buscarPedidoId(id);
-        return ResponseEntity.status(HttpStatus.OK).body(pedidoResponseDTO);
+        EntityModel<PedidoResponseDTO> pageResponseDTO = pedidoAssembler.toPedidoResponseDTO(pedidoResponseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pageResponseDTO);
     }
 
     @GetMapping
-    public ResponseEntity<Page<PedidoResumoResponseDTO>> buscarComFiltros(
-            @RequestParam(required = false) String nome, @RequestParam(required = false) String status,
+    public ResponseEntity<PagedModel<EntityModel<PedidoResumoResponseDTO>>> buscarComFiltros(
+            @RequestParam(required = false) String status,
             @RequestParam(required = false) String cliente, @RequestParam(required = false) LocalDateTime data,
             Pageable pageable) {
 
-        PedidoFiltro filtro = new PedidoFiltro(nome, status, cliente, data);
+        PedidoFiltro filtro = new PedidoFiltro(status, cliente, data);
         Page<PedidoResumoResponseDTO> pedidoResumoResponseDTOS = pedidoService.buscarComFiltros(filtro, pageable);
-
-        return ResponseEntity.status(HttpStatus.OK).body(pedidoResumoResponseDTOS);
+        PagedModel<EntityModel<PedidoResumoResponseDTO>> produtoResumoDTO = pedidoAssembler.toProdutoResumoDTO(pedidoResumoResponseDTOS, filtro);
+        return ResponseEntity.status(HttpStatus.OK).body(produtoResumoDTO);
     }
 }
